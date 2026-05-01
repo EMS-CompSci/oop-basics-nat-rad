bf6aa25c62cefaa8a49ba38466b7bb8d2aeda398
nat-rad <natrat0043@icloud.com>
2026-05-01 12:57:12 +0000

    new distance

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
+#include <cmath>
 #include <string>
 
 class Point {
-
 private:
     double x;
     double y;
 
 public:
-    Point(double initialX, double initialY) {
-        x = initialX;
-        y = initialY;
-    }
+    Point(double initialX, double initialY)
+        : x(initialX), y(initialY) {}
 
     double getX() const {
         return x;
@@ -21,28 +19,35 @@ public:
         return y;
     }
 
-    std::string toString() {
-        return "(" + std::to_string(x) + ", "
-                   + std::to_string(y) + ")";
+    std::string toString() const {
+        return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
     }
 
-
-
-};  
-
+    double distanceTo(const Point &anotherPoint) const {
+        double x1 = getX();
+        double y1 = getY();
+        double x2 = anotherPoint.getX();
+        double y2 = anotherPoint.getY();
+        double xdif = x2 - x1;
+        double ydif = y2 - y1;
+        return std::sqrt(xdif * xdif + ydif * ydif);
+    }
+};
 
 int main() {
-
     Point p1(0.0, 0.0);
     Point p2(3.0, 4.0);
+    Point p3(26.0, 34.5);
 
     std::cout << "p1 = " << p1.toString() << std::endl;
     std::cout << "p2 = " << p2.toString() << std::endl;
+    std::cout << "p3 = " << p3.toString() << std::endl;
 
     std::cout << "p2.x = " << p2.getX() << std::endl;
-    std::cout << "p2.y = " << p2.getY() << std::endl;
+    std::cout << "p3.y = " << p3.getY() << std::endl;
 
-
+    double distance = p1.distanceTo(p2);
+    std::cout << "distance " << distance << std::endl;
 
     return 0;
 }
